3c4eaa8db509cf1cd9f11997e0941f5810f922eb
kan024 <146775606+kan024@users.noreply.github.com>
2023-12-01 14:36:15 -0800

    Update NumberSorterTests.java

--- NumberSorterTests.java
+++ NumberSorterTests.java
@@ -4,25 +4,22 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class NumberSorterTests {
+	@Test
+	public void testEven() {
+        	ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
+		
+        	ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(2, 8, 6));
+		
+		assertArrayEquals(expected, EvenSorter.Even(list);
+	}
+	@Test
+	public void testEvenCount() {
+        	ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
+		
+        	int expected = 3;
+		
+		assertArrayEquals(expected, EvenSorter.evenCount(list);
+	}
 
-    @Test
-    public void evenCountTest() {
-        // this method checks the number of even numbers in the array
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
-        EvenSorter numberSorter = new EvenSorter(list);
-	int expected = numberSorter.Even().size();
-	int result = numberSorter.evenCount();
-	assertArrayEquals(expected, result);
-    }
-@Test
-    public void evenArrayTest() {
-        // this method checks the number of even numbers in the array
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
-        EvenSorter numberSorter = new EvenSorter(list);
-	
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(2, 8, 6));
-	int result = numberSorter.Even();
-	assertArrayEquals(expected, result);
- 
-}
+
 }
