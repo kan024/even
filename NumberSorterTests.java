387e81e15e27cdbde4e5e5a9da89b8e1a5366d26
kan024 <146775606+kan024@users.noreply.github.com>
2023-12-01 14:10:17 -0800

    Update NumberSorterTests.java

--- NumberSorterTests.java
+++ NumberSorterTests.java
@@ -9,8 +9,8 @@ public class NumberSorterTests {
     public void evenCountTest() {
         // this method checks the number of even numbers in the array
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
-        NumberSorter numberSorter = new EvenSorter(list);
-	int expected = numberSorter.Even().length();
+        EvenSorter numberSorter = new EvenSorter(list);
+	int expected = numberSorter.Even().size();
 	int result = numberSorter.evenCount();
 	assertArrayEquals(expected, result);
     }
@@ -18,7 +18,7 @@ public class NumberSorterTests {
     public void evenArrayTest() {
         // this method checks the number of even numbers in the array
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
-        NumberSorter numberSorter = new EvenSorter(list);
+        EvenSorter numberSorter = new EvenSorter(list);
 	
         ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(2, 8, 6));
 	int result = numberSorter.Even();
