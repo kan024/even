6d7002f62d078f8e141d4aaa64f05587ed2dd9bb
kan024 <146775606+kan024@users.noreply.github.com>
2023-12-01 13:36:16 -0800

    Update NumberSorterTests.java

--- NumberSorterTests.java
+++ NumberSorterTests.java
@@ -6,26 +6,22 @@ import java.util.ArrayList;
 public class NumberSorterTest {
 
     @Test
-    public void checkSize() {
+    public void evenCountTest() {
         // this method checks the number of even numbers in the array
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
-        NumberSorter numberSorter = new NumberSorter(numbersList);
-	int expected = 
-	assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2).toArray());
- 
-public class ListExamplesTests {
-	@Test(timeout = 500)
-	public void testMerge1() {
-    		List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
-		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
-		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2).toArray());
-	}
+        NumberSorter numberSorter = new NumberSorter(list);
+	int expected = numberSorter.Even().length();
+	int result = numberSorter.evenCount();
+	assertArrayEquals(expected, result);
+	    
+@Test
+    public void evenArrayTest() {
+        // this method checks the number of even numbers in the array
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8, 11, 6));
+        NumberSorter numberSorter = new NumberSorter(list);
 	
-	@Test(timeout = 500)
-        public void testMerge2() {
-		List<String> l1 = new ArrayList<String>(Arrays.asList("a", "b", "c"));
-		List<String> l2 = new ArrayList<String>(Arrays.asList("c", "d", "e"));
-		assertArrayEquals(new String[]{ "a", "b", "c", "c", "d", "e" }, ListExamples.merge(l1, l2).toArray());
-        }
-
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(2, 8, 6));
+	int result = numberSorter.Even();
+	assertArrayEquals(expected, result);
+ 
 }
